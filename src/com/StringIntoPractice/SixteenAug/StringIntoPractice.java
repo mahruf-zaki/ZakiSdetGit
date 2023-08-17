04ae65cf874ad435f6ce4f9bc8fc4d4177ef34f0
Mahruf Zaki <mahrufzaki@Mahrufs-Air.lan>
2023-08-16 21:07:12 -0400

    it is all about sachin and lara

--- src/com/StringIntoPractice/SixteenAug/StringIntoPractice.java
+++ src/com/StringIntoPractice/SixteenAug/StringIntoPractice.java
@@ -1,2 +1,52 @@
-package com.StringIntoPractice.SixteenAug;public class StringIntoPractice {
+package com.StringIntoPractice.SixteenAug;
+
+public class StringIntoPractice {
+
+    public static void main(String[]args){
+        StringPrac();
+
+    }
+
+    //StringPractice
+    public static void StringPrac(){
+        String hiString = "Hello";
+        String Texas = "can not be a";
+        String Cali = " too hot ";
+        String hotCities = Texas + Cali;
+        String sachin = "great";
+        String lara = "lara";
+        String sOne = " sachin is a great player";
+        String sTwo = "lara is a player with great batting style";
+        String jamunaBridge = "560000000";
+        String shurmaBridge = "990";
+
+
+
+
+
+        //System.out.println(hotCities);
+        //System.out.println(hotCities.toUpperCase());
+        //System.out.println(hotCities.toLowerCase());
+        //System.out.println(sTwo.charAt(10));
+        //System.out.println(jamunaBridge.compareTo(shurmaBridge));
+        //System.out.println(sachin.concat(lara));
+        //System.out.println(sOne.contains(sachin));
+        //System.out.println(sTwo.contains(lara));
+        //System.out.println(sOne.contains(sachin));
+        //System.out.println(sOne.contentEquals(sachin));
+        //System.out.println(sOne.endsWith(sachin));
+        //System.out.println(jamunaBridge.equals(shurmaBridge));
+        //System.out.println(sOne.trim());
+        //System.out.println(shurmaBridge.equalsIgnoreCase(jamunaBridge));
+        //System.out.println(jamunaBridge.getChars(990));
+        //System.out.println(sTwo.indexOf(lara));
+        //System.out.println(sOne.isEmpty());
+        //System.out.println(jamunaBridge.lastIndexOf("560"));
+        // System.out.println(sTwo.lastIndexOf("style"));
+        //System.out.println(sOne.matches(sTwo));
+        //System.out.println(sOne.replace(sachin,Texas));
+        //System.out.println(jamunaBridge.length());
+        //System.out.println(sOne.replaceFirst(sachin, Texas));
+        //System.out.println(sOne.replaceAll(sachin, Cali));
+    }
 }
